36d257cb38ec53f4591eb9001f8ba24ae06ec603
spicytealravioli <Jonnylevy22@gmail.com>
2017-10-07 12:35:30 +0100

    Topic 1

---
+++ vb_basics/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnMessage = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter your name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(251, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "By J.Levy 06/10/2017"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(157, 24)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(182, 20)
+        Me.txtName.TabIndex = 2
+        '
+        'btnMessage
+        '
+        Me.btnMessage.Location = New System.Drawing.Point(49, 60)
+        Me.btnMessage.Name = "btnMessage"
+        Me.btnMessage.Size = New System.Drawing.Size(75, 40)
+        Me.btnMessage.TabIndex = 3
+        Me.btnMessage.Text = "Click for message"
+        Me.btnMessage.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(377, 116)
+        Me.Controls.Add(Me.btnMessage)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "My first program in VB"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnMessage As Button
+End Class
---
+++ vb_basics/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnMessage_Click(sender As Object, e As EventArgs) Handles btnMessage.Click
+        'code for the message button
+        'By J.levy 06/10/17
+        Dim name As String
+        name = txtName.Text
+        MsgBox("Welcome to VB " & name)
+    End Sub
+End Class
